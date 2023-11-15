58358553ab34a8859f08f0ff966979864d46cee9
Steve Vaneeckhout <6131869+SteveVaneeckhout@users.noreply.github.com>
2023-11-15 21:16:37 +0100

    Update application startup code for better DPI support.

--- UmbracoTranslationHelper/Program.cs
+++ UmbracoTranslationHelper/Program.cs
@@ -11,9 +11,7 @@ namespace UmbracoTranslationHelper
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.PerMonitorV2);
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            ApplicationConfiguration.Initialize();
             Application.Run(new MainForm());
         }
     }
